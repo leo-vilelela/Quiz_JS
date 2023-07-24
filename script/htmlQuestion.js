c304931f1e4afea686fa919eed958b195fe4304d
CavassinGomes <lucascavassinig@gmail.com>
2023-07-24 17:51:53 -0300

    Inserindo as questões do quiz

---
+++ script/htmlQuestion.js
@@ -0,0 +1,103 @@
+const quizHTML = [
+    {
+      pergunta: "O que significa a sigla 'HTML'?",
+      alternativas: [
+        "a) Hyperlinks and Text Markup Language",
+        "b) Hypertext Markup Language",
+        "c) Home Tool Markup Language",
+        "d) Hyperlink Text Management Language"
+      ],
+      resposta: 1 
+    },
+    {
+      pergunta: "Qual é a tag utilizada para criar um link em HTML?",
+      alternativas: [
+        "a) <a>",
+        "b) <link>",
+        "c) <href>",
+        "d) <url>"
+      ],
+      resposta: 0   
+    },
+    {
+      pergunta: "Qual é a tag correta para criar uma lista não ordenada em HTML?",
+      alternativas: [
+        "a) <ol>",
+        "b) <list>",
+        "c) <ul>",
+        "d) <unordered>"
+      ],
+      resposta: 2 // índice da alternativa correta (c)
+    },
+    {
+      pergunta: "Em HTML, qual tag é usada para definir o cabeçalho de uma tabela?",
+      alternativas: [
+        "a) <head>",
+        "b) <table>",
+        "c) <header>",
+        "d) <th>"
+      ],
+      resposta: 3 // índice da alternativa correta (d)
+    },
+    {
+      pergunta: "Qual é a versão mais recente do HTML no momento desta pergunta?",
+      alternativas: [
+        "a) HTML5",
+        "b) HTMLX",
+        "c) XHTML",
+        "d) HTML2023"
+      ],
+      resposta: 0 
+    },
+    {
+      pergunta: "Como você insere um comentário em código HTML?",
+      alternativas: [
+        "a) <!-- Isso é um comentário -->",
+        "b) // Isso é um comentário //",
+        "c) **Isso é um comentário**",
+        "d) \"Isso é um comentário\""
+      ],
+      resposta: 0 
+    },
+    {
+      pergunta: "Qual é a tag correta para inserir uma quebra de linha em HTML?",
+      alternativas: [
+        "a) <br>",
+        "b) <lb>",
+        "c) <break>",
+        "d) <newline>"
+      ],
+      resposta: 0 
+    },
+    {
+      pergunta: "Qual é a tag usada para definir uma imagem em HTML?",
+      alternativas: [
+        "a) <img>",
+        "b) <picture>",
+        "c) <image>",
+        "d) <imgsrc>"
+      ],
+      resposta: 0 
+    },
+    {
+      pergunta: "Qual atributo é usado para especificar o endereço de destino de um link em HTML?",
+      alternativas: [
+        "a) <link>",
+        "b) <href>",
+        "c) <target>",
+        "d) <destination>"
+      ],
+      resposta: 1 
+    },
+    {
+      pergunta: "Como você define a codificação de caracteres (character encoding) em um documento HTML?",
+      alternativas: [
+        "a) <meta charset=\"utf-8\">",
+        "b) <encoding>utf-8</encoding>",
+        "c) <char>utf-8</char>",
+        "d) <code>utf-8</code>"
+      ],
+      resposta: 0 
+    }
+  ];
+  
